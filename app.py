8c9299392cf23219075bca0cd8e59470a6a6a7b6
Gurleen <smartgurleen5@gmail.com>
2026-05-04 22:40:47 +0530

    pipeline 5

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from src.data_loader import load_raw_data, merge_datasets
 from src.preprocessor import create_user_item_matrix, get_numpy_matrix
 from src.similarity import calculate_similarity
-
+from src.recommender import Recommender
 def main():
     print("🎬 Starting Movie Recommender Pipeline...")
     
@@ -24,6 +24,7 @@ def main():
     print(f"Saving similarity matrix of size {similarity_matrix.shape} to disk...")
     np.save('data/processed/similarity_matrix.npy', similarity_matrix)  # Saving to your processed folder instead of the root!
     
+
     # Extract NumPy array
     numpy_matrix = get_numpy_matrix(user_item_matrix)
 
@@ -31,5 +32,14 @@ def main():
     print("\nData Pipeline Output (First 5 rows):")
     print(merged_data.head())
 
+    # Pipeline Step 5: Generate Recommendations
+    print("\n--- Testing the Recommender Engine ---")
+    rec_engine = Recommender(user_item_matrix, similarity_matrix)
+    
+    # Try getting 5 recommendations for User ID 10
+    top_picks = rec_engine.recommend(target_user_id=10, top_x=5)
+    
+    print("\nTop 5 Movie Recommendations:")
+    print(top_picks)
 if __name__ == "__main__":
     main()
